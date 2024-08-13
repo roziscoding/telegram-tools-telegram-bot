c97cec961478e964d9d6ddbbfe9266963b6297c1
Roz <rogerio.munhoz@lemon.energy>
2024-08-13 17:23:15 -0300

    fix: pass secret to webhookCallback

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import { bot } from "./bot.ts";
 import { webhookCallback } from "./deps.ts";
 
-const handleUpdate = webhookCallback(bot, "std/http");
+const { TELEGRAM_TOKEN } = Deno.env.toObject();
+
+const handleUpdate = webhookCallback(bot, "std/http", {
+  secretToken: TELEGRAM_TOKEN.replace(/[^0-9a-z_]/g, ""),
+});
 
 Deno.serve(async (req) => {
   if (req.method === "POST") {
