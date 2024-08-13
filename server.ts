d71c3ef0bfbeb382852e793a2de0fafa6bc87d30
Roz <rogerio.munhoz@lemon.energy>
2024-08-13 17:21:02 -0300

    fix: remove path checking

--- server.ts
+++ server.ts
@@ -5,13 +5,10 @@ const handleUpdate = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
   if (req.method === "POST") {
-    const url = new URL(req.url);
-    if (url.pathname.slice(1) === bot.token) {
-      try {
-        return await handleUpdate(req);
-      } catch (err) {
-        console.error(err);
-      }
+    try {
+      return await handleUpdate(req);
+    } catch (err) {
+      console.error(err);
     }
   }
   return new Response();
